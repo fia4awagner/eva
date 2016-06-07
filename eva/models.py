13cb3b3150efe204d450792996e69f8e8d151895
Nykrolis <Tobias-Folde@web.de>
2016-06-07 11:03:46 +0200

    Blabla Methoden Blabla Models

--- eva/models.py
+++ eva/models.py
@@ -1,12 +1,130 @@
-from django.db import models
 from passlib.hash import pbkdf2_sha256
+from django.db import models
+from datetime import datetime
+
 
 from settings import LOGGING_DIR
 
-PBKDF2_SHA256_HASROUNDS=20000
+class DraftHeader(models.Model):
+    headerID = models.AutoField(primary_key=True)
+    headerName = models.CharField(max_length=30)
+    autor = models.CharField(max_length=30)
+    fachrichtung = models.CharField(max_length=30)
+    stufe = models.IntergerField() # 1 = Unterstufe, 2 = Mittelstufe, 3 = Oberstufe
+    fach = models.CharField(max_length = 30)
+    erstellungsdatum = models.DateField()
+    beschreibung = models.CharField(max_length = 500)
+    sichtbarkeit = models.BooleanField()
 
-class Design_Umfrage_Knkopf(models.Model):
-    id = models.AutoField(primary_key=True)
+    def add_group(self):
+        pass
+    
+    def update_from_request(self, request):
+        pass
+    
+    def delete_group(self, groupID):
+        pass
+    
+    def create_survey(self):
+        pass
+    
+    @classmethod
+    def getHeader(cls, headerID):
+        return cls.objects.getGroups.get(headerID = headerID)
+    
+    def getGroups(self):
+        return DraftGroups.objects.filter(headerID = self.headerID)
+    
+class DraftGroups(models.Model):
+    headerID = models.ForeignKey(DraftHeader, on_delete=models.CASCADE)
+    groupID = models.IntegerField()
+    text = models.CharField(max_length = 30, default='')
+    
+    def get_questions(self):
+        return DraftQuestion.objects.filter(groupID = self.groupID, headerID = self.headerID)
+    
+    def add_question(self):
+        pass
+    
+    def delete_question(self, questionID):
+        for qu in self.get_questions():
+            if qu.questionID == questionID:
+                qu.delete()
+            if qu.questionID > questionID:
+                qu.questionID = qu.questionID -1
+            else
+                continue
+            qu.save()
+    
+    def update_from_request(self, request):
+        text = request.GET.get('text', '')
+    
+    def add_question_from_pool(self, pool_question):
+        pass
+    
+    
+class DraftQuestion (models.Model):
+    headerID = models.ForeignKey(DraftHeader, on_delete=models.CASCADE)
+    groupID = models.ForeignKey(DraftGroups, on_delete=models.CASCADE)
+    questionID = models.IntegerField()
+    questionText = models.CharField(max_length = 50)
+    answerType = models.CharField(max_length = 4) # Text, J/N, Note
+    
+    @classmethod
+    def get_next_id(cls, header, group):
+        # TODO get max 
+        pass
+    
+def get_draft_model(header_id, group_id=None, qu_id=None):
+    model = DraftHeader.objects.get(header_id=header_id)
+    if not group_id:
+        return model
+    model.get_goup(group_id)
+    if not group_id:
+        return model
+    return model.get_question(qu_id) 
+    
+###############################################################################    
+    
+class ActiveDraftHeader(models.Model):
+    headerID = models.AutoField(primary_key=True)
+    headerName = models.CharField(max_length=30)
+    autor = models.CharField(max_length=30)
+    fachrichtung = models.CharField(max_length=30)
+    stufe = models.IntergerField() # 1 = Unterstufe, 2 = Mittelstufe, 3 = Oberstufe
+    fach = models.CharField(max_length = 30)
+    erstellungsdatum = models.DateField()
+    beschreibung = models.CharField(max_length = 500)
+    startdatum = models.DateField()
+    enddatum = models.DateField()
+    teilnehmer = models.IntegerField()
+
+    @classmethod
+    def getHeader(cls, headerID):
+        return cls.objects.getGroups.get(headerID = headerID)
+    
+    def getGroups(self):
+        return DraftGroups.objects.filter(headerID = self.headerID)
+    
+    
+class AciveDraftGroups(models.Model):
+    headerID = models.ForeignKey(DraftHeader, on_delete=models.CASCADE)
+    groupID = models.IntegerField()
+    groupName = models.CharField(max_length = 30)
+
+    def getQuestions(self):
+        return DraftQuestion.objects.filter(groupID = self.groupID, headerID = self.headerID)
+    
+    
+class ActiveDraftQuestion (models.Model):
+    headerID = models.ForeignKey(DraftHeader, on_delete=models.CASCADE)
+    groupID = models.ForeignKey(DraftGroups, on_delete=models.CASCADE)
+    questionID = models.IntegerField()
+    questionText = models.CharField(max_length = 50)
+    answerType = models.CharField(max_length = 4) # Text, J/N, Note  
+      
+
+###############################################################################
 
 class User (models.Model):
     user = models.CharField(max_length=200,primary_key=True)
@@ -29,16 +147,18 @@ class User (models.Model):
     
     @classmethod
     def to_hash(cls, psw):
-        return pbkdf2_sha256.encrypt(psw, rounds=PBKDF2_SHA256_HASROUNDS)
+        return pbkdf2_sha256.encrypt(psw,rounds=20000)
     
     @classmethod
     def autenticate(cls, user, psw):
         try:
             user_model = cls.objects.get(user=user)
-            if pbkdf2_sha256.verify(psw, user_model.psw):
-                return True
+        if pbkdf2_sha256.Xverify(psw, user_model.psw):
+            return True
         except cls.NOT_FOUND, e:
             pass
         return False
     
-    
+
+class Departments(models.Model, ListabelModel):
+    option = models.CharField(primary_key=True, max_length=20)
